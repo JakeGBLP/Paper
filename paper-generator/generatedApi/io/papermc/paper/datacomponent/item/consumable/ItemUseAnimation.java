f7a6263ce6458ed580edab5fe8f647325ba740c3
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2024-07-05 18:00:34 +0200

    Generators
    Co-authored-by: Owen1212055 <23108066+Owen1212055@users.noreply.github.com>

---
+++ paper-generator/generatedApi/io/papermc/paper/datacomponent/item/consumable/ItemUseAnimation.java
@@ -0,0 +1,21 @@
+package io.papermc.paper.datacomponent.item.consumable;
+
+/**
+ * Represents the hand animation that is used when a player is consuming this item.
+ */
+public enum ItemUseAnimation {
+    // Paper start - Generated/ItemUseAnimation
+    // @GeneratedFrom 1.21.4
+    NONE,
+    EAT,
+    DRINK,
+    BLOCK,
+    BOW,
+    SPEAR,
+    CROSSBOW,
+    SPYGLASS,
+    TOOT_HORN,
+    BRUSH,
+    BUNDLE;
+    // Paper end - Generated/ItemUseAnimation
+}
